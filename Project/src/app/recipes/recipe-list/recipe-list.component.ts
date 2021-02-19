6f429b9c0be1f5be684923642f26e8997e62be0d
Sithanga <sithanga.uwage@gmail.com>
2021-02-19 19:16:50 +0000

    update with dropdown and improvements

--- Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
-      new Recipe('A test recipe', 'this is a simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872')
+      new Recipe('A test recipe', 'this is a simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=960,872'),
+      new Recipe('abc', 'wtf' , 'https://media.self.com/photos/5f189b76c58e27c99fbef9e3/4:3/w_1200%2Cc_limit/blackberry-vanilla-french-toast.jpg')
     ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
